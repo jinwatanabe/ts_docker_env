e72b796c316283081a55488eb6a7b5d652076687
jinwatanabe <jin.watanabe.6g@gmail.com>
2022-06-19 03:05:14 +0900

    Create Typescript Webpack docker environmetn

---
+++ src/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+  target: ['web', 'es5'],
+  entry: "./html/js/ts/index.ts",
+  output: {
+    path: path.join(__dirname, "/html/js/dist"),
+    publicPath: '/html/js/dist/',
+    filename: "main.js"
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "/html/js/dist"),
+    },
+    open: true,
+    host: "localhost",
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./html/index.html",
+    }),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
